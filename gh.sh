a806c9bc092533232d7ec896d44edfc90f18da47
Benny Jonsson <benny.jonsson@lund.se>
2023-07-18 12:40:30 +0200

    Import on build and new from image

--- gh.sh
+++ gh.sh
@@ -1,15 +1,1 @@
-if $3
-then
-    echo "FORECE" 
-    ls
-    rm -f latest.osm.pbf
-    rm -drf /graphhopper/graph-cache/*
-fi
-if [ -f /graphhopper/graph-cache/properties ]
-then
-    echo "Using existing data" 
-else
-    echo "Download http://download.geofabrik.de/$1/$2-latest.osm.pbf" 
-    wget -O latest.osm.pbf http://download.geofabrik.de/$1/$2-latest.osm.pbf 
-fi
 java -Ddw.graphhopper.datareader.file=latest.osm.pbf -jar *.jar server config.yml
